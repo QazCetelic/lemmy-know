1a24ac1e25a324f0a241a14ab671c00b0304841e
main <main@localhost>
2025-06-23 14:54:30 +0200

    Add report reason

--- src/discord.rs
+++ src/discord.rs
@@ -15,6 +15,7 @@ pub async fn send_post_report_notification(client: &WebhookClient, domain: &str,
         }
         _ => { "Empty".to_string() }
     };
+    let report_reason = report.post_report.reason.to_string();
     let user = report.post_creator.actor_id.to_string();
     client.send(|message| message
         .username(USERNAME)
@@ -22,8 +23,9 @@ pub async fn send_post_report_notification(client: &WebhookClient, domain: &str,
             .title(&post_title)
             .description(&post_description)
             .field("Post", &post_url, false)
-            .field("Reports", &view_url, false)
+            .field("Report reason", &report_reason, false)
             .field("Post Author", &user, false)
+            .field("Reports page", &view_url, false)
         ))
         .await
         .map_err(|e| anyhow!(e))?;
@@ -37,16 +39,17 @@ pub async fn send_comment_report_notification(client: &WebhookClient, domain: &s
     let comment_id = report.comment.id;
     let comment_url = format!("https://{domain}/post/{post_id}/{comment_id}");
     let comment = &report.comment.content.to_string();
+    let report_reason = report.comment_report.reason.to_string();
     let user = report.comment_creator.actor_id.to_string();
     client.send(|message| message
         .username(USERNAME)
         .embed(|embed| embed
             .title("Comment on post")
             .description(&comment)
-            .footer(&view_url, None)
             .field("Comment", &comment_url, false)
-            .field("Reports", &view_url, false)
+            .field("Report reason", &report_reason, false)
             .field("Comment Author", &user, false)
+            .field("Reports page", &view_url, false)
         ))
         .await
         .map_err(|e| anyhow!(e))?;
